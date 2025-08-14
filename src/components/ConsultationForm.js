e53df80768ce4265ecfd3652aafd57ff900af6d9
cky30950 <cky30950@gmail.com>
2025-08-14 22:33:00 +0800

    Delete src directory

--- src/components/ConsultationForm.js
+++
@@ -1,5 +0,0 @@
-// Consultation form UI helpers.
-// Re-export parsing and formatting utilities from the consultation manager
-// for ease of use in your HTML templates.
-
-export { parseConsultationDate, formatConsultationDate, formatConsultationDateTime } from '../managers/ConsultationManager.js';
