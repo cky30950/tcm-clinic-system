509286ee63565c1101669268739755a8c4337edc
cky30950 <cky30950@gmail.com>
2026-05-28 17:29:40 +0800

    Add files via upload

--- system.js
+++ system.js
@@ -14302,7 +14302,7 @@ async function printPrescriptionInstructions(consultationId, consultationData = 
                     });
                     let compositionHtml = '';
                     if (formulaCompositions.length > 0) {
-                        compositionHtml += '<div style="margin-top: 4px; font-size: 0.5em;">';
+                        compositionHtml += '<div style="margin-top: 4px; font-size: calc(0.5em + 2pt);">';
                         compositionHtml += formulaCompositions.map(fc => 
                             `<span style="display:inline-block;margin-right:12px;">${window.escapeHtml(fc.name)}：${window.escapeHtml(fc.composition)}</span>`
                         ).join('');
@@ -14443,7 +14443,7 @@ async function printPrescriptionInstructions(consultationId, consultationData = 
                     // 方劑組成橫向排列（每行四個）
                     let compositionHtml = '';
                     if (formulaCompositions.length > 0) {
-                        compositionHtml += '<div style="margin-top: 4px; font-size: 0.5em;">';
+                        compositionHtml += '<div style="margin-top: 4px; font-size: calc(0.5em + 2pt);">';
                         compositionHtml += formulaCompositions.map(fc => 
                             `<span style="display:inline-block;margin-right:12px;">${window.escapeHtml(fc.name)}：${window.escapeHtml(fc.composition)}</span>`
                         ).join('');
